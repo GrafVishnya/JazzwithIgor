3d994541564adf50042738f4fae0dabdf57b042c
PC <adamcount228@mail.ru>
2024-10-03 01:05:39 +0300

    update

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+///*
+//import java.util.Scanner;
+//
+//class Main {
+//    static Scanner scam = new Scanner(System.in);
+//
+//    public static void main(String[] args) {
+//        int a = scam.nextInt();
+//        int b = scam.nextInt();
+//        int N = 52;
+//        double c = ((N + 2) / Math.sqrt(a * b)) - (Math.pow(b,2) / N);
+//        System.out.printf("%.2f",c);
+//    } ЯгеЙша
+//      принт(ЯЙша
+//}*/
+//
+//
+//
+//import java.util.Scanner;
+//
+//class Main{
+//    public static void main(String[] args) {
+//        Scanner jazz = new Scanner(System.in);
+//        String text = jazz.next();
+//        int a = jazz.nextInt();
+//        int b = jazz.nextInt();
+//        String text1 = text.substring(0, a);
+//        String text2 = text.substring(b, text.length());
+//        text = text1 + text2;
+//        System.out.println(text.toLowerCase());
+//    }
+//}
